417283fa17e4193c11210d8de826bf2b5319083e
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-09-09 01:23:53 -0300

    Adding css to index.html and getAllReviews.html

--- public/js/GetAllReviews.js
+++ public/js/GetAllReviews.js
@@ -1,23 +1,49 @@
- window.onload = async () => {
-        const token = localStorage.getItem('token');
+// Seu JavaScript (GetAllReviews.js)
+const token = localStorage.getItem('token');
 
-        const response = await fetch('/api/allReviews', {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-        const reviews = await response.json();
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const response = await fetch('/api/allReviews', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
 
-        const reviewsContainer = document.getElementById('reviews');
+      if (!response.ok) {
+          throw new Error('Erro ao obter revisões');
+      }
+      const reviewsData = await response.json();
 
-        reviews.forEach((review) => {
-          const reviewElement = document.createElement('div');
-          reviewElement.innerHTML = `
-            <h2>${review.title}</h2>
-            <p>Avaliação: ${review.rating}</p>
-            <p>Comentarios: ${review.review}</p>
-            <p>Special Rating: ${review.specialRating}</p>
-          `;
-          reviewsContainer.appendChild(reviewElement);
-        });
-      };
+      const reviewsContainer = document.getElementById('reviews');
+
+      reviewsData.forEach((review) => {
+          const table = document.createElement('table');
+          table.classList.add('table');
+
+          const tbody = document.createElement('tbody');
+
+          const titleRow = document.createElement('tr');
+          const titleCell = document.createElement('td');
+          titleCell.textContent = `Título: ${review.title}`;
+          titleRow.appendChild(titleCell);
+          tbody.appendChild(titleRow);
+
+          const ratingRow = document.createElement('tr');
+          const ratingCell = document.createElement('td');
+          ratingCell.textContent = `Avaliação: ${review.rating}`;
+          ratingRow.appendChild(ratingCell);
+          tbody.appendChild(ratingRow);
+
+          const commentRow = document.createElement('tr');
+          const commentCell = document.createElement('td');
+          commentCell.textContent = `Comentário: ${review.review}`;
+          commentRow.appendChild(commentCell);
+          tbody.appendChild(commentRow);
+
+          table.appendChild(tbody);
+          reviewsContainer.appendChild(table);
+      });
+  } catch (error) {
+      console.error('Erro ao buscar revisões:', error);
+  }
+});
