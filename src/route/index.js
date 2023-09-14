46916fe3c52c7d35eb4edecfeefcd6cd5fc4614e
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-09-14 15:25:52 -0300

    Removing auth lock

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
-const Auth0Lock = require('auth0-lock');
-const lock = new Auth0Lock('98Vu4GGbLqr3q5EzRPV52lxAQswGHsAf', 'https://dev-q0cc14eebp14ttye.us.auth0.com');
 
 
 router.get('/', function (req, res, next) {
@@ -19,28 +17,12 @@ router.get('/logout', (req, res) => {
   res.oidc.logout({ returnTo: '/' });
 });
 
-router.get('/profile', (req, res) => {
-  if (req.oidc.isAuthenticated()) {
-    const { accessToken } = req.oidc;
-    lock.getUserInfo(accessToken, function(error, profile) {
-      if (!error) {
-        res.render('profile', {
-          userProfile: JSON.stringify(profile, null, 2),
-          title: 'Profile page',
-          bio: profile['https://critix-402174fcc250.herokuapp.com/bio'],
-          familyName: profile['https://critix-402174fcc250.herokuapp.com/familyName'],
-          givenName: profile['https://critix-402174fcc250.herokuapp.com/givenName']
-        });
-      } else {
-      }
-    });
-  } else {
-    res.redirect('/login');
-  }
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
 });
-
-
-
 
 router.get("/signup", (req, res) => {
   res.oidc.login({
