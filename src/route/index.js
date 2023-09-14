30b15357030dfe4649fe49cc63127b9807ab9889
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-09-14 17:09:43 -0300

    Adding lock auth 0

--- src/route/index.js
+++ src/route/index.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
 
-
-
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'CritiX',
     isAuthenticated: req.oidc.isAuthenticated()
   });
 });
-
-
 
 router.get('/profile', requiresAuth(), function (req, res, next) {
   res.render('profile', {
