98430bc142470e42a9b59f7a033d27ec55286edb
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-10-08 14:54:22 -0300

    Fixing db url

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: 'postgres://jhhvyfqc:hqnLHXkkN0IvzNVXszxHcjeKJxb2RMfs@silly.db.elephantsql.com/jhhvyfqc'
 });
 pool.on('connect', () => {
   console.log('Base de Dados conectado com sucesso!');
