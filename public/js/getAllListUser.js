0981c45c5692fe85e194a75348e76ab04418cde2
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-10-19 22:57:55 -0300

    Changing styles from getuser

--- public/js/getAllListUser.js
+++ public/js/getAllListUser.js
@@ -30,6 +30,11 @@ document.addEventListener('DOMContentLoaded', async () => {
                 const tbody = document.createElement('tbody');
 
                 const nameRow = document.createElement('tr');
+                nameRow.addEventListener('click', function(){
+                    localStorage.setItem('listId', list.id);
+                    window.location.href = '/getListById';
+                })
+
                 const nameCell = document.createElement('td');
 
                 const nameText = document.createElement('p');
@@ -44,20 +49,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
                 nameRow.appendChild(nameCell);
                 tbody.appendChild(nameRow);
-
-                const getListButtonRow = document.createElement('tr');
-                const getListButtonCell = document.createElement('td');
-                const getListButton = document.createElement('button');
-                getListButton.id = 'get-button-id';
-                getListButton.textContent = 'Ver Lista';
-                getListButton.addEventListener('click', () => {
-                    localStorage.setItem('listId', list.id);
-                    window.location.href = '/getListById';
-                });
-                getListButtonCell.appendChild(getListButton);
-                getListButtonRow.appendChild(getListButtonCell);
-                tbody.appendChild(getListButtonRow);
-                getListButton.classList.add('btn', 'btn-warning', 'text-dark', 'btn-link', 'mt-3');
 
                 table.appendChild(tbody);
                 listContainer.appendChild(table);
--- public/js/getWatchlist.js
+++ public/js/getWatchlist.js
@@ -57,8 +57,7 @@ document.addEventListener('DOMContentLoaded', async () => {
           movieContainer.appendChild(movieLink);
           listContainer.appendChild(movieContainer);
 
-          randomMovieButton.textContent = 'Aperte aqui para uma surpresa!'
-          randomMovieButton.classList.add('btn-primary', 'btn', 'text-warning')
+          randomMovieButton.innerHTML = '<i class="fa-solid fa-dice" style="color: #000000;"></i>';
           randomMovieButton.href = '/getMovieByTitle'
           randomMovieButton.addEventListener('click', function (event) {
             event.preventDefault();
