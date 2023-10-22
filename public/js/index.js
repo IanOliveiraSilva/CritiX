36c4bde802441f683224b44522fdda880b6684c3
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-10-22 20:04:49 -0300

    Adding login confirm

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,38 @@
 document.getElementById('logout').addEventListener('click', function () {
   const confirmLogout = confirm('Tem certeza que deseja sair da conta?');
   if (confirmLogout) {
-      localStorage.clear();
-      alert('Logout bem-sucedido.');
+    localStorage.clear();
+    alert('Logout bem-sucedido.');
   }
 });
 
-  window.onload = function () {
-    const username = localStorage.getItem('username');
-    if (username) {
-      document.getElementById('username-display').textContent = `Olá, ${username}!`;
-      document.getElementById('logout').textContent = `Sair da conta`;
-      document.getElementById('profile').textContent = `Meu Perfil`;
+window.onload = function () {
+  const requireLoginLinks = document.querySelectorAll('.require-login');
+  const username = localStorage.getItem('username');
+
+  requireLoginLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      if (!username) {
+        event.preventDefault();
+        alert('Você precisa estar logado para acessar esta página.');
+        window.location.href = '/login';
+      }
+    });
+  });
+
+  if (username) {
+    document.getElementById('username-display').textContent = `Olá, ${username}!`;
+    document.getElementById('logout').textContent = `Sair da conta`;
+    document.getElementById('profile').textContent = `Meu Perfil`;
 
 
-      document.getElementById('login-display').style.display = 'none';
-      document.getElementById('signup-display').style.display = 'none';
-    } else {
-      document.getElementById('login-display').textContent = `Entre em sua conta`;
-      document.getElementById('signup-display').textContent = `Crie uma conta`;
+    document.getElementById('login-display').style.display = 'none';
+    document.getElementById('signup-display').style.display = 'none';
+  } else {
+    document.getElementById('login-display').textContent = `Entre em sua conta`;
+    document.getElementById('signup-display').textContent = `Crie uma conta`;
 
-      document.getElementById('profile').style.display = 'none';
-      document.getElementById('create-list-display').style.display = 'none';
-    }
-  };
+    document.getElementById('profile').style.display = 'none';
+    document.getElementById('create-list-display').style.display = 'none';
+  }
+};
