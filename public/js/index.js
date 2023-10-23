b11b9d156d5245d3857964c5e174f647f54d9d27
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-10-23 18:44:18 -0300

    Changing index style

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,33 @@ document.getElementById('logout').addEventListener('click', function () {
 });
 
 window.onload = function () {
+  const sectionTitles = document.querySelectorAll('.section-title');
+  const sectionTexts = document.querySelectorAll('.section-text');
+
+  sectionTitles.forEach((title, index) => {
+    setTimeout(() => {
+      title.classList.add('animate__animated', 'animate__fadeInUp');
+    }, index * 200);
+  });
+
+  sectionTexts.forEach((text, index) => {
+    setTimeout(() => {
+      text.classList.add('animate__animated', 'animate__fadeIn');
+    }, index * 200);
+  });
+
+  const menuItems = document.querySelectorAll('.nav-link');
+
+  menuItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.classList.add('animate__animated', 'animate__pulse');
+    });
+
+    item.addEventListener('mouseout', () => {
+      item.classList.remove('animate__animated', 'animate__pulse');
+    });
+  });
+
   const requireLoginLinks = document.querySelectorAll('.require-login');
   const username = localStorage.getItem('username');
 
@@ -35,4 +62,22 @@ window.onload = function () {
     document.getElementById('profile').style.display = 'none';
     document.getElementById('create-list-display').style.display = 'none';
   }
+
+  const navbar = document.querySelector('.navbar');
+  navbar.style.backgroundColor = 'black';
+  navbar.style.color = 'yellow';
+
+  const navItems = document.querySelectorAll('.nav-item');
+  navItems.forEach(item => {
+    item.style.transition = '0.3s';
+    item.addEventListener('mouseenter', () => {
+      item.style.transform = 'scale(1.1)';
+    });
+    item.addEventListener('mous eleave', () => {
+      item.style.transform = 'scale(1)';
+    });
+  });
+
+  const usernameDisplay = document.getElementById('username-display');
+  usernameDisplay.innerHTML = `<i class="fas fa-user"></i> Olá, ${username}!`;
 };
