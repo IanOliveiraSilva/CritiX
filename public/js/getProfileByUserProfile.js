21dac65aa617299b44e9812264aeedf0872b05c0
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-10-24 11:54:39 -0300

    Changing padding and margin in profile

--- public/js/getProfileByUserProfile.js
+++ public/js/getProfileByUserProfile.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             <div class="ul-profile">
             <img class="profile-image" src="${profileData.body.profile.icon ? profileData.body.profile.icon : 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.webp'}" alt="Ícone do perfil do usuário"/>
                 <h1 class="profile-name">${profileData.body.profile.givenname} ${profileData.body.profile.familyname}</h1> 
-                <p class="profile-bio">@${profileData.body.profile.userprofile}</p>
+                <p class="profile-user">@${profileData.body.profile.userprofile}</p>
                 <p class="profile-bio">${profileData.body.profile.bio}</p>
 
                 </div>
