3b1a33a9102d1dc9f3a754063104f4725274e5b4
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-10-26 14:10:39 -0300

    Adding moviesid to userwatchlist

--- src/controller/list.controller.js
+++ src/controller/list.controller.js
@@ -442,18 +442,16 @@ exports.getUserWatchlist = async (req, res) => {
         const lists = await db.query(
             `
             SELECT u.username AS user, 
-            l.name AS list_name, 
-            l.movies AS movie_titles, 
-            l.moviesid,l.description AS list_description, 
-            l.created_at AS Created_At, 
-            CONCAT(up.name, ' ', up.familyName) as name,
+            l.name AS list_name, l.movies AS movie_titles, l.moviesid,l.description AS list_description, l.created_at AS Created_At, CONCAT(up.name, ' ', up.familyName) as name,
             COUNT(DISTINCT movie) AS movies_count
             FROM lists l
             JOIN users u ON l.userId = u.id
-            JOIN user_profile up ON u.id = up.userid,
-            LATERAL unnest(l.moviesids) AS movie
+            JOIN user_profile up ON u.id = up.userid
+            LEFT JOIN 
+            LATERAL unnest(l.movies) AS movie
+            ON true
             WHERE l.name = $1 and up.userprofile = $2
-            GROUP BY u.username, l.moviesid, l.name, l.movies, l.description, l.created_at, up.name, up.familyname;
+            GROUP BY u.username, l.name, l.moviesid,l.movies, l.description, l.created_at, up.name, up.familyname;
             `,
             [name, userprofile]
         );
